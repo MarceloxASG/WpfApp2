8964137adefe023f71f0309962237bb39487074f
Alumno <Alumno@LAB1507-03>
2024-10-01 21:24:24 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using Entity;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            DProducto dInvoice = new DProducto();
+            var invoices = dInvoice.Get();
+
+            dgInvoices.ItemsSource = invoices;
+        }
+
+        private void btnFilterInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            string invoiceNumber = txtInvoiceNumber.Text;
+
+            if (!string.IsNullOrEmpty(invoiceNumber)) 
+            {
+                var productos = new DProducto().Get(invoiceNumber); 
+                dgInvoices.ItemsSource = productos; 
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un número de factura válido.");
+            }
+        }
+    }
+}
---
+++ data/DProducto.cs
@@ -0,0 +1,53 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DProducto
+    {
+        private string connectionString = "Data Source=LAB1507-03\\SQLEXPRESS;Initial Catalog=FacturaDB;User ID=usuario01;Password=123456;";
+
+        public List<Producto> Get(string invoiceNumber = null)
+        {
+            var invoices = new List<Producto>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("GetFacturas", conn)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                
+                if (!string.IsNullOrEmpty(invoiceNumber))
+                {
+                    cmd.Parameters.AddWithValue("@invoice_number", invoiceNumber);
+                }
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var invoice = new Producto
+                        {
+                            InvoiceID = reader.GetInt32(reader.GetOrdinal("invoice_id")),
+                            CustomerID = reader.GetInt32(reader.GetOrdinal("customer_id")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("date")),
+                            Total = reader.GetDecimal(reader.GetOrdinal("total")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("active")),
+                            InvoiceNumber = reader.GetString(reader.GetOrdinal("invoice_number"))
+                        };
+                        invoices.Add(invoice);
+                    }
+                }
+            }
+            return invoices;
+        }
+    }
+
+}
